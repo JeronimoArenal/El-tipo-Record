10cf9e787173fd5d6aba636a7387409b29f62eb0
JeronimoArenal <jarenal1969@gmail.com>
2026-04-20 13:48:30 +0200

    clase persona creada cmo un record manualmente

--- el-tipo-record/src/main/java/com/example/App.java
+++ el-tipo-record/src/main/java/com/example/App.java
@@ -7,8 +7,6 @@ public class App {
 
     public static void main(String[] args) {
 
-        //abstract no se puede instanciar, solo sirve para que otras hereden de ella
-        Persona persona = new Persona();
 
     }
 }
